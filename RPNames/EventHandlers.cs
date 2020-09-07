7b21c65ab304ff13c6566a5f26e1217b580a1d25
babyboucher <48159308+babyboucher@users.noreply.github.com>
2020-09-07 14:23:11 -0500

    No more same names
    Now 2 names cant appear twice in the same round unless you have too little of a list of names

--- RPNames/EventHandlers.cs
+++ RPNames/EventHandlers.cs
@@ -13,11 +13,15 @@
         Random random = new Random();
         public void OnPlayerChangeRole(ChangingRoleEventArgs ev)
         {
-            if (plugin.Config.ClassTitles.ContainsKey(ev.NewRole.ToString()))
+            if (plugin.NickNames.Count < 1)
             {
-                Random random = new Random();
-                int RandomNameNumber = random.Next(1, plugin.Config.HumanNames.Count-1);
-                ev.Player.DisplayNickname = plugin.Config.ClassTitles[ev.NewRole.ToString()] + plugin.Config.HumanNames[(RandomNameNumber)];
+                Refreshnames();
+            }
+            if (plugin.Config.ClassTitles.ContainsKey(ev.NewRole.ToString()))
+            { 
+                int RandomNameNumber = random.Next(1, plugin.NickNames.Count-1);
+                plugin.NickNames.RemoveAt(RandomNameNumber);
+                ev.Player.DisplayNickname = plugin.Config.ClassTitles[ev.NewRole.ToString()] + plugin.NickNames[(RandomNameNumber)];
             }
             if (plugin.Config.SCPSetting )
             {
@@ -30,8 +34,9 @@
             {
                 if (!plugin.Config.DboisSetting)
                 {
-                    int RandomNameNumber = random.Next(1, plugin.Config.HumanNames.Count - 1);
-                    ev.Player.DisplayNickname = plugin.Config.ClassTitles[ev.NewRole.ToString()] + plugin.Config.HumanNames[RandomNameNumber];
+                    int RandomNameNumber = random.Next(1, plugin.NickNames.Count - 1);
+                    plugin.NickNames.RemoveAt(RandomNameNumber);
+                    ev.Player.DisplayNickname = plugin.Config.ClassTitles[ev.NewRole.ToString()] + plugin.NickNames[RandomNameNumber];
                 }
                 else
                 {
@@ -61,6 +66,18 @@
             if (plugin.Config.DeathReset == true)
             {
                 ev.Target.DisplayNickname = null;
+            }
+        }
+
+        public void Refreshnames()
+        {
+            if (plugin.Config.HumanNames == null)
+            {
+                plugin.NickNames = new List<string> { "Hyde", "Hood", "Hull", "Hogan", "Hitchens", "Higgins", "Hodder", "Huxx", "Hester", "King", "Kubiak", "Koker", "Kerrigan", "Kumara", "Knox", "Koufax", "Keagan", "Kestrel", "Future", "Fitzpatrick", "Fontana", "Fenner", "Furyk", "Finch", "Fullbright", "Fassbinder", "Flood", "Fong", "E.Gold", "Fleetwood", "Fugger", "Frost", "Fsik", "Fawcett", "Fishman", "Freeze", "Fissolo", "Foley", "Fairchild", "Freeman", "Decker", "Dobermann", "Dunlop", "Dumont", "Dandridge", "Diamond", "Cyrus", "Craggs", "Crisper", "Cotheran", "Curry", "Conard", "Cutler", "Coggins", "Cates", "Crisp", "Curio", "Murray", "Morrison", "McKay", "Merchant", "Murillo", "Mooney", "Murdock", "Matisse", "Massey", "McGee", "Minter", "Munson", "Oswald", "Ortega", "Ogle", "Orr", "Ogden", "Onassis", "Olson", "Ollenrenshaw", "Silver", "Small", "Stoneking", "Sweeney", "Surrett", "Swiatek", "Sloane", "Stapleton", "Seibert", "Stroud", "Strode", "Stockton", "Bright", "Miller", "Simarded", "Whix", "Tombstone", "Cimmerian", "Conwell", "Clef", "Gears", "Kiryu", "Heiden", "Lloyd", "Marlowe", "Sorts", "Xyank", "Anborough", "LaFerrier", "Iceberg", "Kondraki", "Eskobar", "Anderson", "Ambrose", "Baudin", "Belitrov", "Chao", "Blackwood", "Fitzroy", "Silberescher", "Aktus", "Diaghilev", "Winchell", "Winters", "Walsh", "Whalen", "Watson", "Wooster", "Woodson", "Winthrop", "Wall", "Wight", "Webb", "Woodard", "Wixx", "Wong", "Whesker", "Wolfenstein", "Winchester", "Wire", "Wolf", "Wheeler", "Warrick", "Walcott", "Wilde", "Wexler", "Wells", "Weeks", "Wainright", "Wallace", "Weaver", "Wagner", "Wadd", "Withers", "Whitby", "Woodland", "Woody", "Warren", "Race", "Rooney", "Russo", "Rude", "Roland", "Reader", "Renshaw", "Rossi", "Riddle", "Ripa", "Richter", "Rosenberg", "Romo", "Ramirez", "Reagan", "Rainwater", "Romirez", "Riker", "Riggs", "Redman", "Reinhart", "Redgrave", "Rafferty", "Rigby", "Roman", "Reece", "Abawi", "Allard", "Adara", "Abbott", "Acampora", "Ackerman", "Ackroyd", "Abbington", "Axworthy", "Ainge", "Abernathy", "Atkinson", "Abner", "Abella", "Agholor", "Allred", "Asola", "Abrams", "Acker", "Abrell", "Acuff", "Archer", "Asterio", "Adair", "Albright", "Adelson", "Atwood", "Aguillar", "Adler", "Arrowood", "Arora" };
+            }
+            else
+            {
+                plugin.NickNames = plugin.Config.HumanNames;
             }
         }
     }
