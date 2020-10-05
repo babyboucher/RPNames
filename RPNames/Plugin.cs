e67f4fbf9715305730d525d1adb19abb87dc52d1
babyboucher <48159308+babyboucher@users.noreply.github.com>
2020-10-04 22:13:35 -0500

    versions yes

--- RPNames/Plugin.cs
+++ RPNames/Plugin.cs
@@ -16,7 +16,7 @@ namespace RPNames
     {
         public override string Name { get; } = "Rp Names";
         public override string Author { get; } = "Babyboucher20";
-        public override Version Version { get; } = new Version(1, 5, 2);
+        public override Version Version { get; } = new Version(1, 5, 8);
         public override Version RequiredExiledVersion { get; } = new Version(2, 0, 10);
         public override string Prefix { get; } = "RP Names";
 
